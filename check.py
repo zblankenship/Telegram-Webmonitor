06699197536baf74cdcde5d2abb59005ae61b088
Zachary Blankenship <blankenship@protonmail.com>
2021-03-09 21:48:31 -0500

    screen shot capture

--- check.py
+++ check.py
@@ -10,6 +10,9 @@ import datetime
 import urllib
 import math
 import requests
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
 
 load_dotenv('key.env')
 mongo = os.getenv('mongo')
@@ -19,6 +22,10 @@ mydb = myclient["webmonitor"]
 headcol = mydb["websites"]
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 chatids = []
+options = Options()
+options.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"
+driver = webdriver.Firefox(options=options, executable_path="C:\Selenium\geckodriver.exe")
+i = 0
 
 def GetWebsite(link):
     url = Request(link, 
@@ -84,26 +91,36 @@ def get_chat_ids(url):
         chatids.append(x['chats_id_to_notify'])
     return chatids
 
+def get_screenshot(url, i):
+    driver.get(url)
+    time.sleep(1)
+    path = 'C:\\Users\\zackb\\Documents\\Projects\\Telegram-Webmonitor\\screenshots\\{}.png'.format(i)
+    print(path)
+    driver.get_screenshot_as_file(path)
+    driver.quit
 
-while True:
-    print("loop")
+
+#while True:
+#    print("loop")
+#    try:
+headdoc = headcol.find()
+start = time.process_time()
+for x in headdoc:
+    req = Request(url=x['url'])
+    get_screenshot(x['url'], i)
+    i = i + 1
     try:
-        headdoc = headcol.find()
-        start = time.process_time()
-        for x in headdoc:
-            req = Request(url=x['url'])
-            try:
-                response = urlopen(req).read()
-            except urllib.error.HTTPError as e:
-                if e.code in (..., 403, ...):
-                    continue
-            newHash = GenerateHash(response)
-            if newHash == x['hash']: 
-                updatetime(x['url'])
-            else: 
-                ChangeNotification(x['url'], token)
-                updatehash(x['url'],newHash)
-        time_taken = SleepTime(start)
-    except Exception as e: 
-        print("error")
-        continue
+        response = urlopen(req).read()
+    except urllib.error.HTTPError as e:
+        if e.code in (..., 403, ...):
+            continue
+    newHash = GenerateHash(response)
+    if newHash == x['hash']: 
+        updatetime(x['url'])
+    else: 
+        ChangeNotification(x['url'], token)
+        updatehash(x['url'],newHash)
+time_taken = SleepTime(start)
+#    except Exception as e: 
+#        print("error")
+#        continue
